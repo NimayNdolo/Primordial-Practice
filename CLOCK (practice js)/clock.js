b60b9be6c6ddf64a063bec8fef87e8ab8f45d484
Nimay Ndolo <nemendolo@gmail.com>
2021-01-17 19:29:43 -0500

    init

---
+++ CLOCK (practice js)/clock.js
@@ -0,0 +1,15 @@
+const morning = 
+const noon = 
+const evening =
+const night = 
+
+const showCurrentTime = function() {
+
+    const clock = document.getElementById("clock");
+    const currentTime = newDate();
+
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const seconds = currentTime.getSeconds();
+    const meridian =
+}
