4e756f79d272cb0cc52f48267d3baf1934d8796f
Nimay Ndolo <nemendolo@gmail.com>
2021-01-19 12:41:08 -0500

    init

--- CLOCK (practice js)/clock.js
+++ CLOCK (practice js)/clock.js
@@ -1,31 +1,40 @@
-var morning = 6;
-var noon = 12;
-var evening = 17;
+var morning = 6; 
+var noon = 12; 
+var evening = 17; 
 var night = 20;
 
-var showCurrentTime = function() {
+var showCurrentTime = function() 
+{ 
+  var clock = document.getElementById('clock'); 
 
-    var clock = document.getElementById('clock');
-    var currentTime = newDate();
+  var currentTime = new Date(); 
 
-    var hours = currentTime.getHours();
-    var minutes = currentTime.getMinutes();
-    var seconds = currentTime.getSeconds();
-    var meridian = "AM";
+  var hours = currentTime.getHours(); 
+  var minutes = currentTime.getMinutes(); 
+  var seconds = currentTime.getSeconds(); 
+  var meridian = "AM";
 
-        if (hours >= noon) {
-            meridian = "PM";
-        }
-        if (hours > noon) {
-            hours = noon - 12;
-        }
-        if (minutes > 10) {
-            minutes = "0" + minutes;
-        }
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
-var clockTime = hours + " : " + minutes + " : " +  seconds + "  " + meridian;
-
-clock.innerText = clockTime;
+    if (hours >= noon) {
+        meridian = "PM";
+    }
+    if (hours > noon) {
+        hours = noon - 12;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+  var clockTime = hours + " : " + minutes + " : " + seconds + " " + meridian; 
+  clock.innerText = clockTime;
 }
+showCurrentTime(); 
+
+var updateClock = function() 
+{
+  var time = new Date().getHours();
+  
+  showCurrentTime();
+}
+updateClock();
