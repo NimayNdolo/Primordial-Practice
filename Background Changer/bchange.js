72ec98fe34c73a4f1527a20e793117e5d11c1232
Nimay Ndolo <nemendolo@gmail.com>
2021-01-20 18:45:32 -0500

    init

---
+++ Background Changer/bchange.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body')
+const button = document.querySelector('button')
+const colors = ['red', 'blue', 'green', 'yellow']
+
+button.addEventListener('click to change', changeBackground);
+
+function changeBackground() {
+    
+}
